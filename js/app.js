00bdc0e62b0ba5fc0e92c00e0df2fb19a070b89b
Michael George <mgeorge9492@gmail.com>
2018-10-09 16:37:22 -0700

    Ater tasks complete, no CSS yet

--- js/app.js
+++ js/app.js
@@ -1,21 +1,62 @@
 'use strict';
 
-alert('Welcome! Let\'s play a guessing game so you can learn more about me.');
+alert('Welcome! Thank you for viewing my webpage. We are going to start with an ice-breaking quiz so you can get to know me!');
 
-var username = prompt('What is your name?');
+//Prompt for username and greeting
+var username = prompt('Let us start by learning YOUR name. What is your name?');
+alert('Welcome, ' + username + '! Let\'s get started on the quiz!');
 
-alert('Welcome, ' + username + '! Let\'s get started!');
-
+//Question 1
 var answerOne = prompt('Do I like to play video games?').toLowerCase();
 
-if (answerOne === 'yes' || answerOne === 'y') {
-    //Tell them! 
-    alert('Ding Ding Ding! You bet I do.');
-    //console.log result
-    console.log('The user got Question 1 right.');
+if (answerOne === 'yes' || answerOne === 'y') { 
+  alert('Ding Ding Ding! You bet I do.');
+  console.log('The user got Question 1 right.');
 } else {
-    //Tell them!
-    alert('Well you\'re wrong... so...')
-    //console.log result
-    console.log('The user got Question 1 wrong.');
-}
+  alert('Well you\'re wrong... so...');
+  console.log('The user got Question 1 wrong.');
+} 
+
+//Question 2
+var answerTwo = prompt('Do I enjoy Spongebob still as an adult?').toLowerCase();
+
+if (answerTwo === 'yes' || answerTwo === 'y') { 
+  alert('Admittedly, yes... yes I do. Don\'t hate!');
+  console.log('The user got Question 1 right.');
+} else {
+  alert('Yeah... totally not a Spongebob fan... (Just kidding, I love Spongebob and you are wrong!!).');
+  console.log('The user got Question 1 wrong.');
+} 
+
+//Question 3
+var answerThree = prompt('Do I think Le\'veon Bell of the Pittsburg Steelers should sign his contract?').toLowerCase();
+
+if (answerThree === 'yes' || answerThree === 'y') { 
+  alert('Let\'s put it this way... for $800k per week I would sign just about anything.');
+  console.log('The user got Question 1 right.');
+} else {
+  alert('You are wrong! He is crushing my fantasy team this year by not playing...');
+  console.log('The user got Question 1 wrong.');
+} 
+
+//Question 4
+var answerFour = prompt('Do I believe there is life in Outer Space?').toLowerCase();
+
+if (answerFour === 'yes' || answerFour === 'y') { 
+  alert('Yeah. It would be hard not to believe there isn\'t.');
+  console.log('The user got Question 1 right.');
+} else {
+  alert('No. I watch far too much \'Ancient Aliens\' to not believe.');
+  console.log('The user got Question 1 wrong.');
+} 
+
+//Question 5
+var answerFive = prompt('Is my fiance smarter than me?').toLowerCase();
+
+if (answerFive === 'yes' || answerFive === 'y') { 
+  alert('You are smart to say yes. She is probably listening.');
+  console.log('The user got Question 1 right.');
+} else {
+  alert('RUN. HIDE. SHE KNOWS YOU SAID THAT.');
+  console.log('The user got Question 1 wrong.');
+} 
