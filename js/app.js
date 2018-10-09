c499c0047d93c418161e80350139ebe46f9db982
Michael George <mgeorge9492@gmail.com>
2018-10-09 14:50:12 -0700

    Instructor Tutorial. One question done for Lab 2.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 'use strict';
 
-alert('IT IS ALIVE!!!');
+alert('Welcome! Let\'s play a guessing game so you can learn more about me.');
+
+var username = prompt('What is your name?');
+
+alert('Welcome, ' + username + '! Let\'s get started!');
+
+var answerOne = prompt('Do I like to play video games?').toLowerCase();
+
+if (answerOne === 'yes' || answerOne === 'y') {
+    //Tell them! 
+    alert('Ding Ding Ding! You bet I do.');
+    //console.log result
+    console.log('The user got Question 1 right.');
+} else {
+    //Tell them!
+    alert('Well you\'re wrong... so...')
+    //console.log result
+    console.log('The user got Question 1 wrong.');
+}
